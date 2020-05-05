0b095b3ecf6336bb7e65f04b73e5bed6518e1853
Seth Moore <sethdmoore@gmail.com>
2020-05-05 02:15:46 -0400

    Add credit for understanding encoding fix-length structs over the wire

--- types/types.go
+++ types/types.go
@@ -6,3 +6,10 @@ type Packet struct {
 	Modifiers uint16
 	KeyCode   uint8
 }
+
+// God bless Vladimir Vivien. This article was paramount in my understanding
+// of serializing bytes over the wire. Not only does the author show you the
+// manual process, they also show you the easy way with structs afterwards.
+
+// https://medium.com/learning-the-go-programming-language/encoding-data-with-the-go-binary-package-42c7c0eb3e73
+// https://archive.vn/jY7tm
